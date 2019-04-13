f4701f5911e920d6b889329a6425a2cdffa71434
Jonathan Hori <jhori333@gmail.com>
2019-04-13 16:39:27 -0400

    adding traversal

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ _spending_your_money_a_flowchart/
 """
 
 import os
-os.chdir('/Users/jonathanhori/Desktop/personal-finace-flowchart')
+os.chdir('/Users/jonathanhori/Desktop/personal-finance-flowchart')
 import pandas as pd
 
 node_file = 'nodes.xlsx'
+YES = ['yes', 'y', '1']#, 'YES', 'Yes']
+NO = ['no', 'n', '0']#, 'NO', 'No']
 
 ###############################################################################
 class Node:
@@ -38,6 +40,15 @@ class Node:
     
     def prompt(self):
         return self.title
+    
+    def keys(self):
+        """I like the .keys() method for dicts, so I'm adding this to nodes"""
+        return vars(self)
+    
+    def display(self):
+        print()
+        print(self.title)
+        print(self.description)
         
     
 class Question(Node):
@@ -51,33 +62,75 @@ class Question(Node):
     
     def get_next(self, answer):
         answer = answer.lower()
-        if answer in ['yes', 'y']:
+        if answer in YES:
             return self.next_yes
         else:
             return self.next_no
-    
+        
+    def display(self):
+        print()
+        print('#################')
+        print(self.title)
+        print(self.description)
+        return input('Yes/No?').lower()
         
 class Graph:
-    def __init__(self):
+    def __init__(self, node_dict):
         self.head = None
+        self.data = node_dict
+        self.head = self.return_node(0)
 
-    def add_node(self, node):
-        """Adds one node to the existing graph from a line of the file"""
-        if self.head is None:
-            self.head = node
-        else:
-            return
-        
+#    def add_node(self, node):
+#        """Adds one node to the existing graph from a line of the file"""
+#        if self.head is None:
+#            self.head = node
+#        else:
+#            return
+            
     def return_node(self, node_id):
         """Returns a pointer to the node with the given id number"""
-        return
+        assert isinstance(node_id, int) or isinstance(node_id, float), \
+            'Must pass integer'
+        node_id = int(node_id)
+        return self.data.get(node_id)
         
-    def from_dict(self, flowchart_dict):
-        id = 0
-        """Creates a flowchart graph from a pre-existing dict of nodes"""
-        while id >= 0:  # Node -1 is the beginning and end of the chart
-            current_node = flowchart_dict[id]
-        return
+#    def from_dict(self, flowchart_dict):
+#        id = 0
+#        """Creates a flowchart graph from a pre-existing dict of nodes"""
+#        while id >= 0:  # Node -1 is the beginning and end of the chart
+#            current_node = flowchart_dict[id]
+#            self.add_node(current_node)
+#            next_node_id = current_node.next
+#            if isinstance(next_node_id, list):
+#                return
+#            
+#        return
+    
+    def user_traverse(self):
+        """Start from the head of the graph, begin taking user input after each
+        question and lead them through using their answers"""
+        node = self.head
+        while node is not None:
+            user_input = node.display()
+            if user_input:
+                if user_input in YES:
+                    node = self.return_node(node.next_yes)
+                elif user_input in NO:
+                    node = self.return_node(node.next_no)
+                else:
+                    print('Please select yes or no')
+            else:
+                node = self.return_node(node.next)
+#                print('##############')
+                if not isinstance(node, Question):
+                    cont_input = input('Continue? ')
+                    if cont_input in YES:
+                        continue
+                    elif user_input in NO:
+                        return
+                    else:
+                        print('Please select yes or no')
+        
 
 ###############################################################################
 def read_data(data_file):
@@ -112,4 +165,9 @@ def create_flowchart_dict(node_dataframe):
 ###############################################################################
 if __name__ == "__main__":
     node_dataframe = read_data(node_file)
-    flowchart = create_flowchart(node_dataframe)
+    flowchart_dict = create_flowchart_dict(node_dataframe)
+    gr = Graph(flowchart_dict)
+    
+    
+    
+    
